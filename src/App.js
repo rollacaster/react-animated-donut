306ba20ef92b4d845d993946f5df2a0450524f3f
Thomas Sojka <thomas.sojka@comsysto.com>
2017-06-20 07:57:57 +0200

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from 'react'
 import { arc, pie } from 'd3-shape'
 
+const radius = 45
+
 class AnimatedDonut extends PureComponent {
   constructor(props) {
     super(props)
@@ -8,17 +10,17 @@ class AnimatedDonut extends PureComponent {
 
     this.state = {
       pathLengths: props.data.map(({ value }) => ({
-        current: value / sum * 45 * Math.PI * 2 * 2,
-        end: value / sum * 45 * Math.PI * 2
+        current: value / sum * radius * Math.PI * 2 * 2,
+        end: value / sum * radius * Math.PI * 2
       }))
     }
   }
 
   render() {
-    const { data } = this.props
+    const { data, speed, width } = this.props
 
     const arcs = pie().value(d => d.value).sort(_ => undefined)(data)
-    const drawArc = arc().outerRadius(45).innerRadius(45)
+    const drawArc = arc().outerRadius(radius).innerRadius(radius)
     const { pathLengths } = this.state
     const { children } = this.props
 
@@ -26,25 +28,24 @@ class AnimatedDonut extends PureComponent {
       clearInterval(this.interval)
     }
 
-    window.requestAnimationFrame(time => {
-      this.setState(({ pathLengths }) => {
-        if (pathLengths.some(offset => offset.current > offset.end)) {
-          return {
-            pathLengths: pathLengths.map(
-              (offset, i) =>
-                offset.current > offset.end &&
-                  (i === 0 ||
-                    (pathLengths[i - 1] &&
-                      pathLengths[i - 1].current <= pathLengths[i - 1].end))
-                  ? { ...offset, current: offset.current - 5 }
-                  : offset
-            )
-          }
-        } else {
-          return this.state
-        }
-      })
+    window.requestAnimationFrame(() => {
+      this.setState(
+        ({ pathLengths }) =>
+          pathLengths.some(offset => offset.current > offset.end)
+            ? {
+                pathLengths: pathLengths.map(
+                  (offset, i) =>
+                    offset.current > offset.end &&
+                      (i === 0 ||
+                        pathLengths[i - 1].current <= pathLengths[i - 1].end)
+                      ? { ...offset, current: offset.current - speed }
+                      : offset
+                )
+              }
+            : this.state
+      )
     })
+
     return (
       <div style={{ width: '50%' }}>
         <svg viewBox="0 0 100 100">
@@ -55,7 +56,7 @@ class AnimatedDonut extends PureComponent {
                 d={drawArc(arc)}
                 style={{
                   stroke: arc.data.color,
-                  strokeWidth: 5,
+                  strokeWidth: width,
                   strokeDasharray: pathLengths[i].end * 2,
                   strokeDashoffset: pathLengths[i].current
                 }}
@@ -75,7 +76,9 @@ AnimatedDonut.defaultProps = {
     { value: 10, color: '#f0e387' },
     { value: 30, color: '#d6d6d6' },
     { value: 70, color: '#43b4a1' }
-  ]
+  ],
+  speed: 5,
+  width: 5
 }
 
 export default AnimatedDonut
