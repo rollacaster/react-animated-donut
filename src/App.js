5feb91b3aa8d6e96b6598c0db05f9a2b4f7180d6
Thomas Sojka <thomas.sojka@comsysto.com>
2017-06-20 07:35:54 +0200

    Animate for arbritrary lengths

--- src/App.js
+++ src/App.js
@@ -2,29 +2,32 @@ import React, { Component } from 'react'
 import { arc, pie } from 'd3-shape'
 
 class AnimatedDonut extends Component {
-  state = {
-    strokeDashoffsets: [45 * Math.PI, 45 * Math.PI, 45 * Math.PI, 45 * Math.PI]
+  constructor(props) {
+    super(props)
+    const sum = props.data.reduce((a, b, i) => a + b.value, 0)
+
+    this.state = {
+      pathLengths: props.data.map(({ value }) => ({
+        current: value / sum * 45 * Math.PI * 2 * 2,
+        end: value / sum * 45 * Math.PI * 2
+      }))
+    }
   }
 
   render() {
-    const data = [
-      { value: 25, color: '#ED696C' },
-      { value: 25, color: '#f0e387' },
-      { value: 25, color: '#d6d6d6' },
-      { value: 25, color: '#43b4a1' }
-    ]
-    const arcs = pie().value(d => d.value)(data)
+    const { data } = this.props
+
+    const arcs = pie().value(d => d.value).sort(_ => undefined)(data)
     const drawArc = arc().outerRadius(45).innerRadius(45)
-    const { strokeDashoffsets } = this.state
+    const { pathLengths } = this.state
     const { children } = this.props
 
-    if (strokeDashoffsets.every(offset => offset <= 0)) {
-      console.log('done')
+    if (pathLengths.every(offset => offset.current <= offset.end)) {
       clearInterval(this.interval)
     }
 
     return (
-      <div style={{ width: '30%' }}>
+      <div style={{ width: '50%' }}>
         <svg viewBox="0 0 100 100">
           <g transform="translate(50, 50)">
             {arcs.map((arc, i) => (
@@ -33,9 +36,9 @@ class AnimatedDonut extends Component {
                 d={drawArc(arc)}
                 style={{
                   stroke: arc.data.color,
-                  strokeWidth: 2,
-                  strokeDasharray: 45 * Math.PI,
-                  strokeDashoffset: strokeDashoffsets[i]
+                  strokeWidth: 5,
+                  strokeDasharray: pathLengths[i].end * 2,
+                  strokeDashoffset: pathLengths[i].current
                 }}
               />
             ))}
@@ -47,24 +50,29 @@ class AnimatedDonut extends Component {
   }
 
   componentDidMount() {
-    this.interval = setInterval(() =>
-      window.requestAnimationFrame(
-        () =>
-          this.setState(({ strokeDashoffsets }) => ({
-            strokeDashoffsets: strokeDashoffsets.map(
-              (offset, i) =>
-                offset > 45 * Math.PI / 2 &&
-                  (i === 0 ||
-                    (strokeDashoffsets[i - 1] &&
-                      strokeDashoffsets[i - 1] <= 45 * Math.PI / 2))
-                  ? offset - 1
-                  : offset
-            )
-          })),
-        10
-      )
-    )
+    this.interval = setInterval(() => {
+      this.setState(({ pathLengths }) => ({
+        pathLengths: pathLengths.map(
+          (offset, i) =>
+            offset.current > offset.end &&
+              (i === 0 ||
+                (pathLengths[i - 1] &&
+                  pathLengths[i - 1].current <= pathLengths[i - 1].end))
+              ? { ...offset, current: offset.current - 1 }
+              : offset
+        )
+      }))
+    }, 5)
   }
+}
+
+AnimatedDonut.defaultProps = {
+  data: [
+    { value: 50, color: '#ED696C' },
+    { value: 10, color: '#f0e387' },
+    { value: 30, color: '#d6d6d6' },
+    { value: 70, color: '#43b4a1' }
+  ]
 }
 
 export default AnimatedDonut
