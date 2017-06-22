45ca101b1deec47d16ea5dd74ae42f43d7c411dd
Thomas Sojka <thomas.sojka@comsysto.com>
2017-06-22 08:52:17 +0200

    Fix main entry

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # react-animated-donut
+
 Animated Donut Chart using react &amp; d3
+
+
+## Usage
+
+```js
+<Donut
+  data={[
+    { value: 50, color: '#ED696C' },
+    { value: 10, color: '#f0e387' },
+    { value: 30, color: '#d6d6d6' },
+    { value: 70, color: '#43b4a1' }
+  ]}
+  speed={5}
+  width={5}
+/>
+```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Donut from './animated-donut.js'
-ReactDOM.render(<Donut />, document.getElementById('root'))
+import Donut from './AnimatedDonut'
+ReactDOM.render(
+  <Donut
+    data={[
+      { value: 50, color: '#ED696C' },
+      { value: 10, color: '#f0e387' },
+      { value: 30, color: '#d6d6d6' },
+      { value: 70, color: '#43b4a1' }
+    ]}
+    speed={5}
+    width={5}
+  />,
+  document.getElementById('root')
+)
